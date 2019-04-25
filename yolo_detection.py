20862b8fafa5ea533617171f8645bfd1179e6c50
takeshikondo <31342274+takeshikondo@users.noreply.github.com>
2019-04-25 16:41:10 +0900

    Add files via upload

---
+++ yolo_detection.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import cv2
+import numpy as np
+from PIL import Image
+
+from yolo import YOLO, detect_video
+#from tiny_yolo import YOLO, detect_video
+from yolo_video import detect_img_2019
+
+def yolo_detection():
+    
+    cam = cv2.VideoCapture(0)
+
+    if not cam.isOpened():
+        sys.exit()
+        
+    yolo = YOLO()
+        
+    while True:
+        _, img = cam.read()
+
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(img_rgb)
+        
+        r_image = detect_img_2019(yolo, image)
+    
+        r_img = np.array(r_image)
+        r_img_bgr = cv2.cvtColor(r_img, cv2.COLOR_RGB2BGR)
+        
+        cv2.imshow('Push Enter Key.', r_img_bgr)
+        
+        if cv2.waitKey(1) == 13:
+            break
+ 
+    yolo.close_session()
+    cam.release()
+    cv2.destroyAllWindows()        
+        
+    
+if __name__ == '__main__':
+    
+    yolo_detection()
+     
